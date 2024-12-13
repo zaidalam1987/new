d71b9e61b355be51aa6ec4756c2e4d9cb8f9ee61
zaidalam1987 <zaidalam390@gmail.com>
2024-12-13 19:14:27 +0500

    done

---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+import { CarouselDemo } from "./components/Demo/CarouselDemo"
+import { ShimmerButtonDemo } from "./components/Demo/ShimmerButtonDemo"
+function App() {
+ 
+
+  return (
+    <>
+      <Button>Click me</Button>
+     <div className = "px-20"> <CarouselDemo/>
+     </div>
+     <ShimmerButtonDemo/>
+    </>
+  )
+}
+
+export default App
